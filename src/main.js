cecf63ac1cdbe41fb1771be15b7d64bd006560d4
Paula <paulafernandezteves@gmail.com>
2021-02-23 19:00:20 -0500

    proyecto final lol

--- src/main.js
+++ src/main.js
@@ -1,6 +1,205 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import {orderAZ, orderZA , orderHigher,orderLower, rolesCampeones, formula} from './data.js';
+//import { mostrarDif } from './data_2.js';
 
-console.log(example, data);
+import data from './data/lol/lol.js';
+//import {rolCampeon} from './data_2.js';
+
+
+
+let traerCampeones = data.data;
+let arrayChamps = Object.values(traerCampeones);
+//console.log(traerCampeones);//
+const mostrarCampeones = document.getElementById("mostrarPersonajes");
+
+
+//Mostrar Galería
+
+const mostrarImagen = (dataImg) => {
+
+  dataImg.forEach(campeones => {
+
+    mostrarCampeones.innerHTML +=
+    `<div id ="${campeones.id}" class="diff-${campeones.info.difficulty}">
+    <figure>
+    <a href="#${campeones.key}" class="modal-card" >
+    <img src="${campeones.splash}" class="splash" id="${campeones.id}"
+    title = "${campeones.title}" slot = "${campeones.blurb}"
+    tags = "${campeones.tags}">
+    </a>
+    <figcaption>${campeones.id}</figcaption>
+    </figure>
+    </div>
+
+    <section id="${campeones.key}" class="modalDialog">
+    <section>
+      <a href="#close" title="Close" class="close">X</a>
+      <img src="${campeones.splash}" class="imgModal">
+      <h2 id="title">${campeones.title}</h2>
+      <h1 id="name">${campeones.name}</h1>
+      <p id="description">${campeones.blurb}</p>
+      <p id="rol">${campeones.tags}</p>
+
+      <table class="default">
+
+      <tr>
+        <th>stats</td>
+        <th>per lvl</td>
+        <th>lvl 1</td>
+        <th>lvl 3</td>
+      </tr>
+
+      <tr>
+        <th>hp</td>
+        <td>${campeones.stats.hpperlevel}</td>
+        <td>${campeones.stats.hp}</td>
+        <td>${formula(514.4, 80, 3)}</td>
+      </tr>
+
+    </table>
+
+    </section>`;
+
+
+  })
+}
+
+mostrarImagen(arrayChamps) ;
+
+//Buscador
+let btn = document.getElementById("btn");
+let mostrarInputBusqueda = document.getElementById("keyword");
+
+const filtrarBusqueda = () => {
+
+  mostrarCampeones.innerHTML = '';
+  //console.log(mostrarInputBusqueda.value); //Aquí no Lee Console
+
+  const texto = mostrarInputBusqueda.value.toLowerCase();
+  for(let campeones of arrayChamps){
+    let name = campeones.name.toLowerCase();
+    if(name.indexOf(texto) !== -1){
+      mostrarCampeones.innerHTML +=
+    `<div id ="${campeones.id}" class="diff-${campeones.info.difficulty}">
+    <figure>
+    <a href="#${campeones.key}" class="modal-card" >
+    <img src="${campeones.splash}" class="splash" id="${campeones.id}"
+    title = "${campeones.title}" slot = "${campeones.blurb}"
+    tags = "${campeones.tags}">
+    </a>
+    <figcaption>${campeones.id}</figcaption>
+    </figure>
+    </div>
+
+    <section id="${campeones.key}" class="modalDialog">
+    <section>
+      <a href="#close" title="Close" class="close">X</a>
+      <img src="${campeones.splash}" class="imgModal">
+      <h2 id="title">${campeones.title}</h2>
+      <h1 id="name">${campeones.name}</h1>
+      <p id="description">${campeones.blurb}</p>
+      <p id="rol">${campeones.tags}</p>
+
+    </section>`;
+  }
+}
+if (mostrarCampeones.innerHTML === ''){
+  mostrarCampeones.innerHTML +=
+    `<div>
+    <p>Campeon no encontrado</p>
+    </div>`;
+}
+}
+
+mostrarInputBusqueda.addEventListener("keyup", filtrarBusqueda);
+
+btn.addEventListener('click', filtrarBusqueda);
+
+//DIFICULTAD//
+const mostrarDificultad = document.getElementById("difficult-select");
+
+const filtroOrderMayorMenor =() =>{
+  mostrarCampeones.innerHTML = '';
+  const valorDificultad = mostrarDificultad.value;
+// console.log(valorDificultad);
+  for(let campeones of orderHigher(arrayChamps)){
+  if(valorDificultad === "higher"){
+    mostrarCampeones.innerHTML += `<div id ="${campeones.id}" class="diff-${campeones.info.difficulty}">
+    <figure>
+    <img src="${campeones.splash}" class="splash">
+    <figcaption>${campeones.id} - ${campeones.info.difficulty}</figcaption>
+    </figure>
+    </div>`;
+  }
+}
+   if(valorDificultad === "low"){
+    for(let campeones of orderLower(arrayChamps)){
+    //let arrayOrderNM = orderLower(arrayChamps);
+   //console.log(arrayOrderNM);
+    mostrarCampeones.innerHTML += `<div id ="${campeones.id}" class="diff-${campeones.info.difficulty}">
+    <figure>
+    <img src="${campeones.splash}" class="splash">
+    <figcaption>${campeones.id} - ${campeones.info.difficulty}</figcaption>
+    </figure>
+    </div>`;
+  }
+}
+}
+mostrarDificultad.addEventListener('change', filtroOrderMayorMenor);
+//console.log(arrayOrderMN);
+
+
+
+//ORDENAR A-Z//
+
+const mostrarOrder = document.getElementById("selectLetter");
+
+const filtrarOrder = () => {
+  mostrarCampeones.innerHTML = '';
+  const valorOrder = mostrarOrder.value;
+  //const valorOrderZA = mostrarOrderZA.value;
+ // console.log(valorOrder);
+  //console.log(valorOrderZA);
+
+    if (valorOrder === "orderAZ"){
+      let arrayOrderAZ = orderAZ(arrayChamps);
+       mostrarImagen(arrayOrderAZ)
+    }
+
+    if (valorOrder === "orderZA"){
+      let arrayOrderZA = orderZA(arrayChamps);
+        //let arrayOrderZA = orderZA(arrayChamps);
+        mostrarImagen(arrayOrderZA)
+    }
+  }
+
+mostrarOrder.addEventListener('change', filtrarOrder);
+
+
+
+//ROLES
+
+
+const rolesPorCampeones = document.querySelectorAll(".roles_li");
+
+rolesPorCampeones.forEach(boton => {
+	boton.addEventListener("click", (e) => {
+    const roles = e.target.value;
+      console.log(roles);
+
+      mostrarCampeones.innerHTML = '';
+    if (roles === "All"){
+      mostrarImagen(arrayChamps)
+    }
+     // if (roles === roles)
+     else{
+        let filtroRol = rolesCampeones(arrayChamps, roles);
+        mostrarImagen(filtroRol)
+    }
+  })
+});
+//console.log(rolesPorCampeones)
+///////////////////////////////////
+
+
+
+
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,144 @@
-import { example, anotherExample } from '../src/data.js';
+import { suma, orderAZ, orderZA, orderHigher, orderLower, rolesCampeones, filtrarBusqueda} from '../src/data.js';
 
 
-describe('example', () => {
+/*describe('Filtro de ordenAZ', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof orderAZ).toBe('function');
   });
 
-  it('returns `example`', () => {
+  it('returns ``', () => {
+    const filter = []
     expect(example()).toBe('example');
   });
-});
+});*/
 
 
-describe('anotherExample', () => {
+describe('Formula Sumar', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof suma).toBe('function');
   });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('returns `4`', () => {
+    expect(suma(2 , 2)).toEqual(4);
   });
 });
+
+//Retorna solo si el name.value del objeto inician con Mayusculas
+//o si empiezan todos los name.valueen minuscula
+//Pero crea error si los name.valuepossen combinanciones de ambas
+describe('Formula orderAZ', () => {
+  it('is a function', () => {
+    expect(typeof orderAZ).toBe('function');
+  });
+  it('returns `chogath,lux,shaco,udyr`', () => {
+    const data = [
+      { name: "lux" },
+      { name: "udyr" },
+      { name: "chogath" },
+      { name: "shaco" },
+    ];
+    const dataAZ = [
+      { name: "chogath" },
+      { name: "lux" },
+      { name: "shaco" },
+      { name: "udyr" },
+    ];
+    expect(orderAZ(data)).toEqual(dataAZ);
+  });
+});
+// Orden de forma Z-A
+describe('Formula orderZA', () => {
+  it('is a function', () => {
+    expect(typeof orderZA).toBe('function');
+  });
+  it('returns `Viktor,Riven,Kayle,Anivia`', () => {
+    const data = [
+      { name: "Riven" },
+      { name: "Anivia" },
+      { name: "Viktor" },
+      { name: "Kayle" },
+    ];
+    const dataAZ = [
+      { name: "Viktor" },
+      { name: "Riven" },
+      { name: "Kayle" },
+      { name: "Anivia" },
+    ];
+    expect(orderZA(data)).toEqual(dataAZ);
+  });
+});
+//Ordenar por Dificultad ALTO-BAJO
+//Los nombres deben estar escritos tal cual
+describe('Formula Alto-Bajo', () =>{
+  it("is a function", () => {
+    expect(typeof orderHigher).toBe('function');
+  });
+  it('returns difficulty`9,8,4,3`' ,() => {
+    const data = [
+      { name: "Aatrox", info  :{difficulty : 4} },
+      { name: "Elise" , info :{difficulty : 9} },
+      { name: "maokai", info  :{difficulty : 8}  },
+      { name: "Fiora" , info  :{difficulty : 3} },
+    ];
+    const dataHigher = [
+      { name: "Elise", info  :{difficulty : 9} },
+      { name: "maokai" , info :{difficulty : 8} },
+      { name: "Aatrox", info  :{difficulty : 4}  },
+      { name: "Fiora" , info  :{difficulty : 3} },
+    ];
+
+  expect(orderHigher(data)).toEqual(dataHigher);
+  });
+});
+//Ordenar por Dificultad BAJO-ALTO
+describe('Formula Bajo-Alto', () =>{
+  it("is a function", () => {
+    expect(typeof orderLower).toBe('function');
+  });
+  it('returns difficulty`3,5,7,9`' ,() => {
+    const data = [
+      { name: "Amumu", info  :{difficulty : 3} },
+      { name: "FiddleSticks" , info :{difficulty : 9} },
+      { name: "Talon", info  :{difficulty : 7}  },
+      { name: "Tryndamere" , info  :{difficulty : 5} },
+    ];
+    const dataLower = [
+      { name: "Amumu", info  :{difficulty : 3} },
+      { name: "Tryndamere" , info :{difficulty : 5} },
+      { name: "Talon", info  :{difficulty : 7}  },
+      { name: "FiddleSticks" , info  :{difficulty : 9} },
+    ];
+
+  expect(orderLower(data)).toEqual(dataLower);
+  });
+});
+
+// Filtro de POR SUS Roles(Tags)
+// Solo funciona si Tag posee solo un elemento por Array
+describe('Filtro de los Roles de los Campeones', () =>{
+  it('is a function', () =>{
+    expect(typeof rolesCampeones).toBe('function');
+  });
+  it('returns para Support`Leona, Kayle, Lulu`', () =>{
+    const data =[
+      { name : "Leona", tags: "Support" },
+      { name : "Kayle", tags:  "Support" },
+      { name : "Lucian", tags: "Marksman" },
+      { name : "Lulu", tags: "Support" },
+    ];
+    const dataTag=[
+      { name : "Leona", tags: "Support" },
+      { name: "Kayle", tags : "Support" },
+      { name: "Lulu", tags:  "Support" },
+    ];
+    expect(rolesCampeones(data, 'Support')).toEqual(dataTag);
+  });
+});
+
+//Filtro Barra de Busqueda
+describe('Filtrar en el Buscador por el nombre del Campeon', () =>{
+  it('is a function', () =>{
+    expect(typeof filtrarBusqueda).toBe('function');
+  })
+})
+
+
